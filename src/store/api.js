871ebc52532caefe015eabf523b00ab360baa02b
Alex <alexyakovenko2@gmail.com>
2018-05-20 22:39:22 +0300

    commit config

--- src/store/api.js
+++ src/store/api.js
@@ -1,3 +1,8 @@
 export default {
-  login: 'http://api.stud.hz/?action=login'
+  login: 'http://api.stud.hz/?action=login',
+  // login: process.env.API_URL + '/login',
+  // logout: process.env.API_URL + '/logout',
+  // register: process.env.API_URL + '/register',
+  // products: process.env.API_URL + '/products',
+  // product: (productId) => (process.env.API_URL + '/products/#{id}').replace('#{id}')
 }
