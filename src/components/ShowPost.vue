2730563f42622e8853a9cbc5f45b1efa326d086e
Alex <kochubey.alexander94@gmail.com>
2018-05-27 23:09:48 +0300

    Created output ShowPost  page

--- src/components/ShowPost.vue
+++ src/components/ShowPost.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="form">
-   <h1>This response: {{showPost}}</h1> 
-    
-  </div>
+  <v-app id="inspire">
+        <v-content style="padding-top: 0;">
+            <v-container fluid grid-list-xl>
+                <v-layout row wrap align-center justify-center>
+                   <v-flex flex xs12 sm10 md10 lg8 xl8>
+                        <v-card class="" hover>
+                            <v-card-media
+                            class="white--text"
+                            height="500px"
+                            :src="showPost.img"
+                            >
+                            </v-card-media>
+                            <v-card-text style="padding: 0;">
+                                <h3>{{ showPost.title }}</h3>
+                                <p style="margin-bottom: 0;">Описание: {{ showPost.text }}</p>
+                                <p style="margin-bottom: 0;">Состояние: {{ showPost.id_status }}</p>
+                                <p style="margin-bottom: 0;">Стоимость: {{ showPost.price }}</p>
+                                <p style="margin-bottom: 0;">Город: {{ showPost.id_city }}</p>
+                                <p style="margin-bottom: 0;">Контактный телефон: {{ showPost.telephone }}</p>
+                                <p style="margin-bottom: 0;">Дата обновления: {{ showPost.updated_at }}</p>
+                                <p style="margin-bottom: 0;">Количество просмотров: {{ showPost.wiews }}</p>
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
@@ -10,10 +34,14 @@ import { mapState } from 'vuex'
 
 export default {
   computed: {
-    ...mapState(['showPost'])
+    ...mapState([
+      'showPost'
+    ])
   },
   created () {
     this.$store.dispatch('showPostLoad', {id: this.$route.params.id})
   },
 }
 </script>
+
+/* {{ showPost.title }} */
