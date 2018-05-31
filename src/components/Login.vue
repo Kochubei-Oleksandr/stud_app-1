3e638e24dbfd98d659ca7e28f438fe9d3006149a
Alex <kochubey.alexander94@gmail.com>
2018-05-31 16:43:49 +0300

    FIX show form login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,7 +10,7 @@
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field prepend-icon="person" name="login" label="Login" v-model="login" type="text"></v-text-field>
+                  <v-text-field prepend-icon="email" name="email" label="Email" v-model="login" type="text"></v-text-field>
                   <v-text-field id="password" prepend-icon="lock" name="password" v-model="password" label="Password" type="password"></v-text-field>
                 </v-form>
               </v-card-text>
