993b2704ce9f13fc37f45603711e5340ab049e0f
Alex <alexyakovenko2@gmail.com>
2018-06-01 23:15:07 +0300

    commit

--- src/components/ShowPost.vue
+++ src/components/ShowPost.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="form">
-   <h1>This response: {{showPost}}</h1> 
+   <h1>This response: {{showPost}}</h1>
+   <h1>This response: {{showPost.id}}</h1>
+   <h1>This response: {{showPost.text}}</h1>
+   <h1>This response: {{showPost.title}}</h1>
+   <h1>This response: {{showPost.id_user}}</h1>
     
   </div>
 </template>
